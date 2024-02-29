099d91af8614521272ffde080522be82acb9584c
Your Name <you@example.com>
2024-02-29 21:30:36 +0200

    Закінув телеграм бота

---
+++ TgBot/common/text_to_speech.py
@@ -0,0 +1,14 @@
+import gtts
+import os
+
+def text_to_sp(text):
+    tss = gtts.gTTS(text, lang='en')
+    audio_path = 'message.mp3'
+    tss.save(audio_path)
+    if os.path.exists(audio_path):
+        print("Audio file was created successfully")
+        return audio_path
+    else:
+        print("Failed to create audio file")
+        return None
+    
